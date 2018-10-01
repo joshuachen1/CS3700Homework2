851fd62de89799a1ef24b16fcac3e56f29be9877
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 11:26:39 -0700

    3rd commit (Method Reference)

--- MethodReferenceExample1.java
+++ MethodReferenceExample1.java
@@ -1,7 +1,14 @@
 public class MethodReferenceExample1 {
     public static void main(String[] args) {
 
-        Thread t = new Thread(() -> printMessage());
+        // Method Reference (NameOfClassMethod :: methodName)
+        // --------------------------------------------------
+        // If there is a lambda expression that
+        //      1. Takes in no input arguments
+        //      2. Calls a method w/o any input arguments
+        // Then you can replace the lambda expression with a method reference
+        //
+        Thread t = new Thread(MethodReferenceExample1::printMessage);   // equivalent to: () -> printMessage()
         t.start();
     }
     public static void printMessage() {
