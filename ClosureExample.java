6fed553281769eed70f676d51ff9042c7a2d4252
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 10:52:17 -0700

    2nd commit

--- ClosureExample.java
+++ ClosureExample.java
@@ -1,11 +1,18 @@
 public class ClosureExample {
     public static void main(String[] args) {
         int a = 10;
+        int b = 20;
+
+        // Since doProcess method is called
+        // The doProcess method calls the implementation of process in the main method
+        // Meaning there is NO variable b within the scope of doProcess
+        // but exists since the Java RunTime keeps track of the value of b
+
         doProcess(a, new Process() {
 
             @Override
             public void process(int i) {
-                System.out.println(i);
+                System.out.println(i + b);
             }
         });
     }
