b2e71ec5dbeb8d4244ebfe7a99159d4e145c9f3e
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 10:28:48 -0700

    3rd commit (DivideByZeroException)

--- ExceptionHandlingExample.java
+++ ExceptionHandlingExample.java
@@ -3,12 +3,12 @@ import java.util.function.BiConsumer;
 public class ExceptionHandlingExample {
     public static void main(String[] args) {
         int[] someNumbers = {1, 2, 3, 4};
-        int key = 2;
+        int key = 0;
 
         // The lambda expression (v, k) -> System.out.println(v + k)
         // Overrides the accept method as one that accepts 2 integers
         // and prints the sum.
-        process(someNumbers, key, (v, k) -> System.out.println(v + k));
+        process(someNumbers, key, (v, k) -> System.out.println(v / k));
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
