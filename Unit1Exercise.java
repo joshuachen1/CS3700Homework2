88b2703c4152ce6c89503018002aaadb77a79970
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 00:29:15 -0700

    second commit (Java 7 Solution)

--- Unit1Exercise.java
+++ Unit1Exercise.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Unit1Exercise {
@@ -13,11 +15,34 @@ public class Unit1Exercise {
         );
 
         // Step 1: Sort list by last name
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+        });
 
         // Step 2: Create a method that prints all elements in the list
+        printAll(people);
 
         // Step 3: Create a method that prints all people that have last name beginning with 'C'
+        printLastNameBeginningWithC(people);
 
+    }
 
+    private static void printAll(List<Person> people) {
+        for (Person p :
+                people) {
+            System.out.println(p);
+        }
+    }
+
+    private static void printLastNameBeginningWithC(List<Person> people) {
+        for (Person p :
+                people) {
+            if (p.getLastName().startsWith("C")) {
+                System.out.println(p);
+            }
+        }
     }
 }
