17eec777e87dcd2f62de7eb7f71c85f2d9dfb854
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 10:48:56 -0700

    1st commit

---
+++ ClosureExample.java
@@ -0,0 +1,20 @@
+public class ClosureExample {
+    public static void main(String[] args) {
+        int a = 10;
+        doProcess(a, new Process() {
+
+            @Override
+            public void process(int i) {
+                System.out.println(i);
+            }
+        });
+    }
+
+    public static void doProcess(int i, Process p) {
+        p.process(i);
+    }
+}
+
+interface Process {
+    void process(int i);
+}
