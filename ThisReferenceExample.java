677fd883b11f1c5aa337e0626658edc7c332d5b2
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 11:15:03 -0700

    4th commit (this reference in non-static method w/ lambda)

--- ThisReferenceExample.java
+++ ThisReferenceExample.java
@@ -4,30 +4,24 @@ public class ThisReferenceExample {
         p.process(i);
     }
 
+    public void execute() {
+        doProcess(10, i -> {
+            System.out.println("Value of i is " + i);
+
+            // This time, this reference refers to the thisReferenceExample instance
+            // or whatever instance of the ThisReferenceExample calls this method.
+            // Rather than an instance of Process had an Anonymous Inner Class been made here.
+            System.out.println(this);
+        });
+    }
+
     public static void main(String[] args) {
         ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
 
-        thisReferenceExample.doProcess(10, new Process() {
+        thisReferenceExample.execute();
+    }
 
-            @Override
-            public void process(int i) {
-                System.out.println("Value of i is " + i);
-
-                // "this" refers to the new instance of Process on line 10
-                System.out.println(this);
-            }
-
-            @Override
-            public String toString() {
-                return "This is the anonymous inner class";
-            }
-        });
-
-        thisReferenceExample.doProcess(10, i -> {
-            System.out.println("Value of i is " + i);
-
-            // Lambda does not override the "this" reference like an anonymous inner class does
-            System.out.println(this);
-        });
+    public String toString() {
+        return "This is the main ThisReferenceExample class instance.";
     }
 }
