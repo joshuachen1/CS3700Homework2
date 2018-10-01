bc53ec7649ddae280df32f13b5402a0e9a559b92
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 11:19:03 -0700

    2nd commit (Threads w/ lambda)

--- MethodReferenceExample1.java
+++ MethodReferenceExample1.java
@@ -1,6 +1,8 @@
 public class MethodReferenceExample1 {
     public static void main(String[] args) {
-        printMessage();
+
+        Thread t = new Thread(() -> printMessage());
+        t.start();
     }
     public static void printMessage() {
         System.out.println("Hello");
