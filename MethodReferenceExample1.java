4c3132a5478546dc78fb21ef27eb5f0b63dff45e
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 11:18:07 -0700

    1st commit

---
+++ MethodReferenceExample1.java
@@ -0,0 +1,8 @@
+public class MethodReferenceExample1 {
+    public static void main(String[] args) {
+        printMessage();
+    }
+    public static void printMessage() {
+        System.out.println("Hello");
+    }
+}
