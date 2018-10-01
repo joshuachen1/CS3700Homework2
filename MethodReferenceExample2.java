0e59ab7f183bf577bb4f9c56be5d78920f1a2712
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 11:32:03 -0700

    1st commit

---
+++ MethodReferenceExample2.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class MethodReferenceExample2 {
+
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carroll", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Matthew", "Arnold", 39)
+        );
+
+        System.out.println("Printing all persons");
+        performConditionally(people, p -> true, p -> System.out.println(p));
+
+    }
+
+    // There are built in Function Interfaces for Out-of-the-Box Interfaces
+    // The Predicate Interface contains the method test that accepts a generic object and returns a boolean
+    // Use the Predicate Interface rather than create an Interface
+    private static void performConditionally(List<Person> people, Predicate<Person> predicate, Consumer<Person> consumer) {
+        for (Person p :
+                people) {
+            if (predicate.test(p)) {
+                consumer.accept(p);
+            }
+        }
+    }
+}
