fe1ba99f0bd479288c31578bdb8f8d6152555314
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 10:27:53 -0700

    2nd commit

--- ExceptionHandlingExample.java
+++ ExceptionHandlingExample.java
@@ -1,15 +1,20 @@
+import java.util.function.BiConsumer;
+
 public class ExceptionHandlingExample {
     public static void main(String[] args) {
         int[] someNumbers = {1, 2, 3, 4};
         int key = 2;
 
-        process(someNumbers, key);
+        // The lambda expression (v, k) -> System.out.println(v + k)
+        // Overrides the accept method as one that accepts 2 integers
+        // and prints the sum.
+        process(someNumbers, key, (v, k) -> System.out.println(v + k));
     }
 
-    private static void process(int[] someNumbers, int key) {
+    private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
         for (int i :
                 someNumbers) {
-            System.out.println(i + key);
+            consumer.accept(i, key);
         }
     }
 }
